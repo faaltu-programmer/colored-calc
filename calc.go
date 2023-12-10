0fb915acdf204b67a21c88c904c97e9ba68dcd54
faaltu-programmer <faaltu-programmer@proton.me>
2023-12-09 19:44:34 -0800

    implements divide functionality

--- calc.go
+++ calc.go
@@ -1,9 +1,24 @@
 package calc
 
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrNotDivisibleByZero = errors.New("not divisible by zero")
+
 func Add(a, b float64) float64 {
 	return a + b
 }
 
 func Multiply(a, b float64) float64 {
 	return a * b
+}
+
+func Divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("calc.Divide: provided %d for denominator %w", b, ErrNotDivisibleByZero)
+	}
+
+	return a / b, nil
 }
