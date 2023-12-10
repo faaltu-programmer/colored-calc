3a65777876d092e70cdae6007f949e7ab82387ef
faaltu-programmer <faaltu-programmer@proton.me>
2023-12-09 18:03:31 -0800

    exports Add identifier

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package calc is meant for learning go features. As such this package is
+// very basic and probably not useful
+//
+// The colored part just sends an instruction which color to use. This is
+// silly.
+package calc
